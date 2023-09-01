7a56e84d71823b3ad2772890ee5f55ea5b5769ac
loryruta <loryruta23@gmail.com>
2023-09-01 14:53:37 +0200

    fsc: Add batching in gen_db

--- font-style-classifier/gen_db.py
+++ font-style-classifier/gen_db.py
@@ -5,6 +5,7 @@ from PIL import Image
 import torchvision.transforms.functional as F
 import pandas as pd
 from model.model import FSC_Encoder
+import torch
 
 FSC_DB_PATH="./.fsc-db"
 FSC_DB_COLLECTION_NAME="embeddings"
@@ -25,7 +26,7 @@ db_client.recreate_collection(
 print("Loading the model...")
 model = FSC_Encoder()
 model.load_checkpoint("./model/latest-checkpoint.pt")
-
+model = model.cuda()
 
 # For every dataset sample, run an inference and save the embedding to the DB
 print("Reading dataset index...")
@@ -33,23 +34,44 @@ df = pd.read_csv(FSC_DATASET_CSV)
 
 dataset_size = len(df)
 
-for i, row in df.iterrows():
-    print(f"{i + 1}/{dataset_size}\t{row['filename']}")
+BATCH_SIZE = 128
 
-    img_path = os.path.join(FSC_DATASET_DIR, row["filename"])
-    x = Image.open(img_path).convert('L')
-    x = F.to_tensor(x)
+batch = []
 
-    y = model(x.unsqueeze(0)).squeeze()
+def upsert_batch(batch_id: int):
+    print(f"Processing batch #{batch_id}...")
 
-    result = db_client.upsert(
-        collection_name=FSC_DB_COLLECTION_NAME,
-        points=[
-            PointStruct(
-                id=i,
-                vector=y.tolist(),
-                payload=row.to_dict() 
+    batched_x = torch.stack(batch)
+    batched_x = batched_x.cuda()
+    batched_y = model(batched_x)
+
+    print(f"Upserting point: ", end="")
+    for j, y in enumerate(batched_y):
+        row_id = batch_id * 128 + j
+        print(f"{row_id}, ", end="")
+
+        db_client.upsert(
+            collection_name=FSC_DB_COLLECTION_NAME,
+            points=[
+                PointStruct(
+                    id=row_id,
+                    vector=y.tolist(),
+                    payload=df.iloc[row_id].to_dict() 
+                )
+            ]
             )
-        ]
-        )
+    print()
 
+
+for i, row in df.iterrows():
+    img_path = os.path.join(FSC_DATASET_DIR, row["filename"])
+    img = Image.open(img_path)
+    x = F.to_tensor(img)
+    batch.append(x)
+
+    if len(batch) >= BATCH_SIZE:
+        upsert_batch()
+        batch = []
+
+if len(batch) > 0:
+    upsert_batch()
