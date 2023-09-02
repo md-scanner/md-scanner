c85312ec17dce19e9a3d70b61db082b3983eb0e4
loryruta <loryruta23@gmail.com>
2023-09-02 14:50:18 +0200

    fsc: Update gen_db.py

--- font-style-classifier/gen_db.py
+++ font-style-classifier/gen_db.py
@@ -1,10 +1,13 @@
 import os
+import time
+import random
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, PointStruct
 from PIL import Image
 import torchvision.transforms.functional as F
 import pandas as pd
 from model.model import FSC_Encoder
+
 import torch
 
 FSC_DB_PATH="./.fsc-db"
@@ -12,6 +15,10 @@ FSC_DB_COLLECTION_NAME="embeddings"
 
 FSC_DATASET_CSV="/home/rutayisire/unimore/cv/md-scanner/fsc-dataset/dataset.csv"
 FSC_DATASET_DIR="/home/rutayisire/unimore/cv/md-scanner/fsc-dataset"
+
+
+input("This action will fresh and regenerate the FSC_Database. Press any key to proceed...")
+
 
 print("Initializing the DB...")
 db_client = QdrantClient(path=FSC_DB_PATH) 
@@ -30,48 +37,95 @@ model = model.cuda()
 
 # For every dataset sample, run an inference and save the embedding to the DB
 print("Reading dataset index...")
-df = pd.read_csv(FSC_DATASET_CSV)
+dataset = pd.read_csv(FSC_DATASET_CSV)
+dataset_length = len(dataset)
 
-dataset_size = len(df)
-
-BATCH_SIZE = 128
+BATCH_SIZE = 100
 
 batch = []
 
-def upsert_batch(batch_id: int):
-    print(f"Processing batch #{batch_id}...")
+class DbGenerator:
+    def __init__(self):
+        self.cur_batch = []
+        self.cur_batch_id = 0
 
-    batched_x = torch.stack(batch)
-    batched_x = batched_x.cuda()
-    batched_y = model(batched_x)
+        # Filter the original dataset to keep only fonts that have italic or bold styles
+        total_fonts = dataset['font'].unique()
+        complete_fonts = dataset[dataset['is_italic'] & dataset['is_bold']]['font'].unique()
+        print(f"Found {len(complete_fonts)}/{len(total_fonts)} complete founds (with italic and bold)...")
+        
+        self.df = dataset[dataset['font'].isin(complete_fonts)]
+        self.sample_count = len(self.df)
+        print(f"Prepared {len(self.df)}/{len(dataset)} samples...")
 
-    print(f"Upserting point: ", end="")
-    for j, y in enumerate(batched_y):
-        row_id = batch_id * 128 + j
-        print(f"{row_id}, ", end="")
 
+    def _generate_embeddings(self):
+        batched_x = torch.stack([img for img, _ in self.cur_batch])
+        batched_x = batched_x.cuda()
+        self.embeddings = model(batched_x)
+
+
+    def _upsert_embeddings(self):
         db_client.upsert(
             collection_name=FSC_DB_COLLECTION_NAME,
             points=[
                 PointStruct(
-                    id=row_id,
-                    vector=y.tolist(),
-                    payload=df.iloc[row_id].to_dict() 
+                    id=random.getrandbits(64),
+                    vector=embedding.tolist(),
+                    payload=payload,
                 )
-            ]
+                for (_, payload), embedding in zip(self.cur_batch, self.embeddings)
+            ],
+            wait=False
             )
-    print()
 
 
-for i, row in df.iterrows():
-    img_path = os.path.join(FSC_DATASET_DIR, row["filename"])
-    img = Image.open(img_path)
-    x = F.to_tensor(img)
-    batch.append(x)
+    def _upsert_batch(self):
+        sample_i = (self.cur_batch_id + 1) * BATCH_SIZE
+        print(f"{sample_i}/{self.sample_count}, batch: {self.cur_batch_id} - ", end="")
 
-    if len(batch) >= BATCH_SIZE:
-        upsert_batch()
-        batch = []
+        # Generate the embeddings for the current batch
+        st = time.time()
+        self._generate_embeddings()
+        dt = time.time() - st
 
-if len(batch) > 0:
-    upsert_batch()
+        print(f"Inference: {dt:.3f}, ", end="")
+
+        # Insert the embeddings into the DB
+        st = time.time()
+        self._upsert_embeddings()
+        el_count = db_client.count(collection_name=FSC_DB_COLLECTION_NAME).count
+        dt = time.time() - st
+
+        print(f"DB insertion: {dt:.3f} (count: {el_count}), ", end="")
+
+        # Done!
+        print("Done!")
+
+        self.cur_batch = []
+        self.cur_batch_id += 1
+
+
+    def fill_db(self):
+        self.cur_batch = []
+        self.cur_batch_id = 0
+
+        print("Filling DB...")
+
+        for i, row in self.df.iterrows():
+            img_path = os.path.join(FSC_DATASET_DIR, row["filename"])
+            img = Image.open(img_path)
+            img = F.to_tensor(img)
+
+            self.cur_batch.append((img, row.to_dict()))
+
+            if len(self.cur_batch) >= BATCH_SIZE:
+                self._upsert_batch()
+
+        if len(self.cur_batch) > 0:
+            self._upsert_batch()
+
+
+if __name__ == "__main__":
+    db_gen = DbGenerator()
+    db_gen.fill_db()
